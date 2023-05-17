977dad18b5272ae2635568b0e9a16948a96855c0
Mika Tammi <mika.tammi@unikie.com>
2023-05-17 17:07:30 +0300

    Simply build all packages of ghaf
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- hydrajobs.nix
+++ hydrajobs.nix
@@ -1,8 +1,5 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {ghaf}: {
-  hydraJobs = {
-    generic-x86_64-debug.x86_64-linux = ghaf.packages.x86_64-linux.generic-x86_64-debug;
-    nvidia-jetson-orin-debug.aarch64-linux = ghaf.packages.aarch64-linux.nvidia-jetson-orin-debug;
-  };
+  hydraJobs = ghaf.packages;
 }
