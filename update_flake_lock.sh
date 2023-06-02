11d53abdd6284cbb0f729a6e7950007169cae2f0
Mika Tammi <mika.tammi@unikie.com>
2023-06-02 13:46:11 +0300

    Update update_flake_lock.sh script
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- update_flake_lock.sh
+++ update_flake_lock.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 curl https://raw.githubusercontent.com/tiiuae/ghaf/main/flake.lock -o flake.lock
 nix flake show
+
+if [[ $(git status --porcelain --untracked-files=no) ]]; then
+	git add flake.lock
+	git commit -s -m "Update flake.lock"
+	git push
+fi
