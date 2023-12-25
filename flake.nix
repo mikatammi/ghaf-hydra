fc1313a4946d337ca276aad4f9da4904133d71cc
Mika Tammi <mika.tammi@unikie.com>
2023-12-25 22:36:24 +0200

    Fix flake.nix
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   description = "Ghaf - Documentation and implementation for TII SSRC Secure Technologies Ghaf Framework";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-new.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-new.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils-new.url = "github:numtide/flake-utils";
     nixos-generators = {
